58456c8e87bb9daf40098eff8aa786045ebaa32a
Ravindra Das <ravindra21075@gmail.com>
2026-06-27 17:40:22 +0530

    Day 6: MongoDB persistent storage working

--- consumer/kafka_consumer.py
+++ consumer/kafka_consumer.py
@@ -1,9 +1,16 @@
 import json
 import redis
 from kafka import KafkaConsumer
+from pymongo import MongoClient
+from datetime import datetime
 
 # Redis connection
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+# MongoDB connection
+mongo = MongoClient('mongodb://localhost:27017/')
+db = mongo['analytics']
+events_collection = db['events']
 
 # Kafka Consumer
 consumer = KafkaConsumer(
@@ -11,10 +18,13 @@ consumer = KafkaConsumer(
     bootstrap_servers='localhost:9092',
     value_deserializer=lambda v: json.loads(v.decode('utf-8')),
     auto_offset_reset='latest',
-    group_id='analytics-group'
+    group_id='analytics-group-v2'
 )
 
 print("Consumer started — listening to Kafka...")
+
+batch = []
+count = 0
 
 for message in consumer:
     event = message.value
@@ -22,19 +32,24 @@ for message in consumer:
     speed = event['speed_kmh']
     status = event['status']
 
-    # City wise driver count
+    # Redis live stats
     r.incr(f"city:{city}:total_events")
-
-    # Speed tracking (running average)
     r.lpush(f"city:{city}:speeds", speed)
-    r.ltrim(f"city:{city}:speeds", 0, 99)  # Last 100 speeds only
-
-    # Status count
+    r.ltrim(f"city:{city}:speeds", 0, 99)
     r.incr(f"city:{city}:status:{status}")
 
-    # Print every 100 messages
-    total = int(r.get(f"city:{city}:total_events") or 0)
-    if total % 100 == 0:
+    # MongoDB batch insert
+    event['saved_at'] = datetime.utcnow()
+    batch.append(event)
+
+    # Insert every 100 events
+    if len(batch) >= 100:
+        events_collection.insert_many(batch)
+        batch = []
+
+    count += 1
+    if count % 500 == 0:
+        total = int(r.get(f"city:{city}:total_events") or 0)
         speeds = r.lrange(f"city:{city}:speeds", 0, -1)
         avg_speed = sum(float(s) for s in speeds) / len(speeds)
-        print(f"City: {city} | Total Events: {total} | Avg Speed: {avg_speed:.1f} km/h")
+        print(f"✅ {count} events | {city} | Avg Speed: {avg_speed:.1f} km/h | MongoDB saved!")

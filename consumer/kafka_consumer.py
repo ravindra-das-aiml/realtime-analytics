5a2325b39112e29b6092482e15203095e1dd4bf1
Ravindra Das <ravindra21075@gmail.com>
2026-07-02 13:14:32 +0530

    Month 3: Local Redis for unlimited requests

--- consumer/kafka_consumer.py
+++ consumer/kafka_consumer.py
@@ -2,12 +2,10 @@ import json
 import redis
 from kafka import KafkaConsumer
 
-# Upstash Redis
+# Local Redis
 r = redis.Redis(
-    host='square-molly-76911.upstash.io',
+    host='localhost',
     port=6379,
-    password='gQAAAAAAASxvAAIgcDFiODQ2ZmRhMzA0YWM0NWIzOTA3NWIyYTY2MmE1YTA1Ng',
-    ssl=True,
     decode_responses=True
 )
 
@@ -16,12 +14,12 @@ consumer = KafkaConsumer(
     bootstrap_servers='localhost:9092',
     value_deserializer=lambda v: json.loads(v.decode('utf-8')),
     auto_offset_reset='latest',
-    group_id='analytics-group-v7',
+    group_id='analytics-group-v9',
     fetch_max_bytes=52428800,
     max_poll_records=500
 )
 
-print("Consumer started — Redis Pipelining enabled...")
+print("Consumer started — Local Redis Pipelining enabled...")
 
 batch = []
 count = 0
@@ -30,7 +28,6 @@ for message in consumer:
     batch.append(message.value)
     
     if len(batch) >= 100:
-        # Redis Pipeline — batch operations (10x faster!)
         pipe = r.pipeline()
         for event in batch:
             city = event['city']
@@ -44,5 +41,5 @@ for message in consumer:
         
         count += len(batch)
         city = batch[-1]['city']
-        print(f"✅ {count} events | {city} | Redis Pipeline executed!")
+        print(f"✅ {count} events | {city} | Local Redis Pipeline executed!")
         batch = []
